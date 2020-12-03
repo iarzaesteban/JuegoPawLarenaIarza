ea33258eaf4ae7561fe1a363112948200199b604
iarzaesteban <iarzaesteban94@gmail.com>
2020-12-03 19:57:28 -0300

    empezando css

--- public/index.php
+++ public/index.php
@@ -3,23 +3,26 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/style/style.css">
     <title>Epidemiology</title>
 </head>
-<body>
-    <h1>Epidemiology</h1>
+<body >
+    <div class="image-fondo" > 
+        <section class="container">
+            <h1>Epidemiology</h1>
 
-    <section>
-        <form method="post" action="" name="signup-form">
-       
-            <label>Usuario</label>
-            <input type="text" name="username" pattern="[a-zA-Z0-9]+" required />
+            <form class="login" method="post" action="" name="signup-form">
+        
+                <label class="lavel-color">Usuario</label>
+                <input class="imput" type="text" name="username" pattern="[a-zA-Z0-9]+" required />
 
-            <label>Password</label>
-            <input type="password" name="password" required />
+                <label class="lavel-color">Password</label>
+                <input class="imput" type="password" name="password" required />
 
-            <button type="submit" name="register" value="register">Log In</button>
-        </form>
+                <button type="submit" name="register" value="register">Log In</button>
+            </form>
+        </section>
 
-    </section>
+    </div>
 </body>
 </html>
