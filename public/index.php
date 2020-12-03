f112cf15584a0ca8cf310d83dcabac8868c1c82c
iarzaesteban <iarzaesteban94@gmail.com>
2020-12-03 18:13:58 -0300

    estructurando proyecto

---
+++ public/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Epidemiology</title>
+</head>
+<body>
+    <h1>Epidemiology</h1>
+
+    <section>
+        <form method="post" action="" name="signup-form">
+       
+            <label>Usuario</label>
+            <input type="text" name="username" pattern="[a-zA-Z0-9]+" required />
+
+            <label>Password</label>
+            <input type="password" name="password" required />
+
+            <button type="submit" name="register" value="register">Log In</button>
+        </form>
+
+    </section>
+</body>
+</html>
