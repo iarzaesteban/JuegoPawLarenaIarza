08a8cfd57d9660233cd5fcf756ce044490c9e93a
NicLarUniversidad <71468011+NicLarUniversidad@users.noreply.github.com>
2020-12-13 16:39:52 -0300

    llamada con ajax

--- public/assets/js/components/HTMLContainerModifier.js
+++ public/assets/js/components/HTMLContainerModifier.js
@@ -14,8 +14,33 @@ class HTMLContainerModifier {
     }
 
     static PostDinamicoFormConResultadoOut(formID, outID, buttonID, url) {
-
+        var div = document.getElementById(formID)
+        if (div) {
+            div.addEventListener("submit", function(ev) {
+                ev.preventDefault()
+                $.ajax({
+                    type: "POST",
+                    url: url,
+                    data: $("#"+formID).serialize(),
+                    dataType: "text",
+                    sucess: function(data) {
+                        var res = document.getElementById(outID)
+                        if (res) {
+                            res.html(data)
+                        } else {
+                            console.error("No se encontró el elemento out: " + outID)
+                        }
+                    },
+                    error: function(http, status, exception) {
+                        console.error(status + " ||| " + exception)
+                    }
+                })
+                return false
+            })
+        } else {
+            console.error("No se encontró el contenedor: " + formID)
+        }
     }
 
-    
+
 }
